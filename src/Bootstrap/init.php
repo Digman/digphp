78c6f242a816185988675e18cb004e785c1f925a
longjinhua <longjinhua@115.com>
2017-05-09 20:28:18 +0800

    init files

---
+++ src/Bootstrap/init.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Application Entry File
+ * Do some initialization
+ */
+
+define('START_TIME', microtime(true));
+
+require dirname(__DIR__) . '/Vendor/autoload.php';
---
+++ src/Public/web/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Default Web Application
+ */
+
+require dirname(dirname(__DIR__)) . '/Bootstrap/init.php';
+
+
+var_dump(class_exists('App\Http\Web\Controllers\TestController', true));
